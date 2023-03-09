293942f29a67f3d3182abca5934f34bfd675e3a8
RadmiloM <markovicradmilo35@gmail.com>
2023-03-09 18:08:11 +0100

    edit and delete method modified in controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,14 +10,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 #[Route('/user')]
 class CategoryController extends AbstractController {
+    
         private $categoryRepository;
 
-        public function __construct(CategoryRepository  $categoryRepository)
+        private $managerRegistry;
+
+
+        public function __construct(CategoryRepository  $categoryRepository,ManagerRegistry $managerRegistry)
         {
             $this->categoryRepository = $categoryRepository;
+            $this->managerRegistry = $managerRegistry;
+
         }
 
      /**
@@ -35,91 +43,94 @@ class CategoryController extends AbstractController {
                $article->setContent($data['content']);
                $category->setArticle($article);
 
-               $entityManager = $this->getDoctrine()->getManager();
+               $entityManager = $this->managerRegistry->getManager();
                $entityManager->persist($category);
                $entityManager->persist($article);
                $entityManager->flush();
 
-               return new JsonResponse(Response::HTTP_CREATED);
+               return new JsonResponse([],Response::HTTP_CREATED);
         }
-         /**
-         * @Route("/category/{id}", name="find_one_category", methods={"GET"})
-         */
-         public function findCategoryById($id): JsonResponse
-            {
-                $category = $this->categoryRepository->findOneBy(['id' => $id]);
 
-                if(!$category) {
-                          return new JsonResponse("category with id: " .$id. " does not exists");
+            /**
+             * @Route("/category/{id}", name="find_one_category", methods={"GET"})
+             */
+        public function findCategoryById($id): JsonResponse
+        {
+            $category = $this->categoryRepository->findOneBy(['id' => $id]);
+
+            if(!$category) {
+                        return new JsonResponse("category with id: " .$id. " does not exists");
+                    }
+                    
+            $data = [
+                'name' => $category->getName(),
+                'title'=> $category->getArticle()->getTitle(),
+                'content'=> $category->getArticle()->getContent()
+            ];
+
+            return new JsonResponse($data,Response::HTTP_OK);
+        }
+
+            /**
+             * @Route("/categories", name="find_all_categories", methods={"GET"})
+             */
+        public function findAllCategories(): JsonResponse 
+        {
+        $categories = $this->categoryRepository->findAll();
+
+        $data = [];
+
+        foreach ($categories as $category) {
+
+            $data[] = [
+                'name' => $category->getName(),
+                'title'=> $category->getArticle()->getTitle(),
+                'content'=>$category->getArticle()->getContent()
+            ];
+        }
+            
+        return new JsonResponse($data, Response::HTTP_OK);
+        }
+
+
+        /**
+        * @Route("/delete/category/{id}", name="delete_category", methods={"DELETE"})
+        */
+        public function deleteCategory($id): JsonResponse
+        {
+            $category = $this->categoryRepository->findOneBy(['id' => $id]);
+            
+            if(!$category) {
+                        return new JsonResponse("category with id: " .$id. " does not exists");
+                    }
+
+            $entityManager = $this->managerRegistry->getManager();
+            $entityManager->remove($category);
+            $entityManager->flush();
+
+            return new JsonResponse(Response::HTTP_OK);
+        }
+
+
+        /**
+        * @Route("/edit/category/{id}", name="update_category", methods={"PUT"})
+        */
+        public function updateCategory($id, Request $request): JsonResponse
+        {
+            $category = $this->categoryRepository->findOneBy(['id' => $id]);
+            if(!$category) {
+                            return new JsonResponse("category with id: " .$id. " does not exists");
                         }
-                        
-                $data = [
-                    'name' => $category->getName(),
-                    'title'=> $category->getArticle()->getTitle(),
-                    'content'=> $category->getArticle()->getContent()
-                ];
 
-                return new JsonResponse($data,Response::HTTP_OK);
-            }
+            $data = json_decode($request->getContent(), true);
 
-          /**
-              * @Route("/categories", name="find_all_categories", methods={"GET"})
-              */
-             public function findAllCategories(): JsonResponse {
-                $categories = $this->categoryRepository->findAll();
+            empty($data['name']) ? true : $category->setName($data['name']);
 
-                $data = [];
+            $entityManager = $this->managerRegistry->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
 
-                foreach ($categories as $category) {
-
-                    $data[] = [
-                        'name' => $category->getName(),
-                        'title'=> $category->getArticle()->getTitle(),
-                        'content'=>$category->getArticle()->getContent()
-                    ];
-                }
-                
-                return new JsonResponse($data, Response::HTTP_OK);
-                }
-
-
-             /**
-                * @Route("/delete/category/{id}", name="delete_category", methods={"DELETE"})
-                */
-               public function deleteCategory($id): JsonResponse
-                {
-                   $category = $this->categoryRepository->findOneBy(['id' => $id]);
-                   
-                   if(!$category) {
-                             return new JsonResponse("category with id: " .$id. " does not exists");
-                           }
-
-                   $this->categoryRepository->removeCategory($category);
-
-                   return new JsonResponse(Response::HTTP_OK);
-               }
-
-
-
-             /**
-                * @Route("/edit/category/{id}", name="update_category", methods={"PUT"})
-                */
-               public function updateCategory($id, Request $request): JsonResponse
-               {
-                   $category = $this->categoryRepository->findOneBy(['id' => $id]);
-                   if(!$category) {
-                                 return new JsonResponse("category with id: " .$id. " does not exists");
-                               }
-
-                   $data = json_decode($request->getContent(), true);
-
-                   empty($data['name']) ? true : $category->setName($data['name']);
-
-                   $this->categoryRepository->updateCategory($category);
-
-                   return new JsonResponse(Response::HTTP_OK);
-               }
-
-
-
+            return new JsonResponse(Response::HTTP_OK);
+        }
+               
 }
