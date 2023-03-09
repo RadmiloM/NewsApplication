66d89931b68392560d9c8914bf9c88684e8f6ef9
RadmiloM <markovicradmilo35@gmail.com>
2023-03-09 16:37:45 +0100

    delete and update method changed in controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 #[Route('/user')]
@@ -17,103 +18,114 @@ class ArticleController extends AbstractController {
 
    
 
-    private $articleRepository;
+        private $articleRepository;
+        private $managerRegistry;
 
-    public function __construct(ArticleRepository $articleRepository){
-        $this->articleRepository = $articleRepository;
-    }
-
-    /**
- * @Route("/articles", name="find_all_articles", methods={"GET"})
- */
-public function findAllArticles(): JsonResponse
-{
-    $articles = $this->articleRepository->findAll();
-    $data = [];
-
-    foreach ($articles as $article) {
-        $data[] = [
-            'title' => $article->getTitle(),
-            'content' => $article->getContent(),
-        ];
-    }
-    
-    return new JsonResponse($data, Response::HTTP_OK);
-}
-
-
-    /**
-     * @Route("/createArticle", name="create_article", methods={"POST"})
-     */
-    public function createArticle(Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $article = new Article();
-        $article->setContent($data['content']);
-        $article->setTitle($data['title']);
-
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($article);
-        $entityManager->flush();
-
-        return new JsonResponse(Response::HTTP_CREATED);
-    }
-
-        /**
-     * @Route("/article/{id}", name="find_one_article", methods={"GET"})
-     */
-    public function findArticleById($id): JsonResponse
-    {
-        $article = $this->articleRepository->findOneBy(['id' => $id]);
-
-        if(!$article){
-            return new JsonResponse("article with id: " .$id. " does not exists");
+        public function __construct(ArticleRepository $articleRepository,ManagerRegistry $managerRegistry){
+            $this->articleRepository = $articleRepository;
+            $this->managerRegistry = $managerRegistry;
         }
 
-        $data = [
-            'title' => $article->getTitle(),
-            'content' => $article->getContent()
-        ];
+        /**
+        * @Route("/articles", name="find_all_articles", methods={"GET"})
+        */
+        public function findAllArticles(): JsonResponse
+        {
+            $articles = $this->articleRepository->findAll();
+            $data = [];
 
-        return new JsonResponse($data, Response::HTTP_OK);
-    }
+            foreach ($articles as $article) {
+                $data[] = [
+                    'title' => $article->getTitle(),
+                    'content' => $article->getContent(),
+                ];
+            }
+            
+            return new JsonResponse($data, Response::HTTP_OK);
+        }
 
-    /**
-     * @Route("/edit/article/{id}", name="update_article", methods={"PUT"})
-     */
-    public function updateArticle($id, Request $request): JsonResponse
-    {
-        $article = $this->articleRepository->findOneBy(['id' => $id]);
+        /**
+         * @Route("/createArticle", name="create_article", methods={"POST"})
+         */
+        public function createArticle(Request $request): JsonResponse
+        {
+            $data = json_decode($request->getContent(), true);
 
-        if(!$article) {
-               return new JsonResponse("article with id: " .$id. " does not exists");
+            $article = new Article();
+            $article->setContent($data['content']);
+            $article->setTitle($data['title']);
+
+            $entityManager = $this->managerRegistry->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return new JsonResponse([],Response::HTTP_CREATED);
+        }
+
+            /**
+         * @Route("/article/{id}", name="find_one_article", methods={"GET"})
+         */
+        public function findArticleById($id): JsonResponse
+        {
+            $article = $this->articleRepository->findOneBy(['id' => $id]);
+
+            if(!$article){
+                return new JsonResponse("article with id: " .$id. " does not exists");
             }
 
-        $data = json_decode($request->getContent(), true);
+            $data = [
+                'title' => $article->getTitle(),
+                'content' => $article->getContent()
+            ];
 
-        empty($data['title']) ? true : $article->setTitle($data['title']);
-        empty($data['content']) ? true : $article->setContent($data['content']);
+            return new JsonResponse($data, Response::HTTP_OK);
+        }
 
-        $this->articleRepository->updateArticle($article);
+        /**
+         * @Route("/edit/article/{id}", name="update_article", methods={"PUT"})
+         */
+        public function updateArticle($id, Request $request): JsonResponse
+        {
+            $article = $this->articleRepository->findOneBy(['id' => $id]);
 
-        return new JsonResponse(Response::HTTP_OK);
-    }
+            if(!$article) {
 
-   /**
-    * @Route("/delete/article/{id}", name="delete_article", methods={"DELETE"})
-    */
-   public function deleteArticle($id): JsonResponse
-    {
-       $article = $this->articleRepository->findOneBy(['id' => $id]);
+                return new JsonResponse("article with id: " .$id. " does not exists");
 
-        if(!$article) {
-                      return new JsonResponse("article with id: " .$id. " does not exists");
-           }
+            }
 
-       $this->articleRepository->removeArticle($article);
+            $data = json_decode($request->getContent(), true);
 
-       return new JsonResponse(Response::HTTP_OK);
-   }
+            empty($data['title']) ? true : $article->setTitle($data['title']);
+            empty($data['content']) ? true : $article->setContent($data['content']);
+
+
+            $entityManager = $this->managerRegistry->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return new JsonResponse(Response::HTTP_OK);
+        }
+
+        /**
+        * @Route("/delete/article/{id}", name="delete_article", methods={"DELETE"})
+        */
+        public function deleteArticle($id): JsonResponse
+            {
+
+            $article = $this->articleRepository->findOneBy(['id' => $id]);
+
+            if(!$article) {
+                        
+                return new JsonResponse("article with id: " .$id. " does not exists");
+            }
+            
+            $entityManager = $this->managerRegistry->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+
+            return new JsonResponse(Response::HTTP_OK);
+
+            }
 
 }
