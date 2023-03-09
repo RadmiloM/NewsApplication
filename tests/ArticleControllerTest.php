e619eed00aea8838d9652cbd43d341727a29771f
RadmiloM <markovicradmilo35@gmail.com>
2023-03-09 18:07:21 +0100

    article repository formatted

--- tests/ArticleControllerTest.php
+++ tests/ArticleControllerTest.php
@@ -15,170 +15,175 @@ use Doctrine\ORM\EntityManagerInterface;
 
 
 
-class ArticleControllerTest extends TestCase
-{
+class ArticleControllerTest extends TestCase {
     public function testingWhenArticleWithProvidedIdIsNotPresentInDatabase(): void {
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+        
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
 
-        $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
+    $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
 
-        $articleRepositoryMock->expects($this->once())
-                              ->method('findOneBy')
-                              ->with(['id' => 1])
-                              ->willReturn(null);
+    $articleRepositoryMock->expects($this->once())
+                            ->method('findOneBy')
+                            ->with(['id' => 1])
+                            ->willReturn(null);
 
-        $response = $articleController->findArticleById(1);
+    $response = $articleController->findArticleById(1);
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertInstanceOf(JsonResponse::class, $response);
 
-        $this->assertEquals('"article with id: 1 does not exists"', $response->getContent());
+    $this->assertEquals('"article with id: 1 does not exists"', $response->getContent());
+
     }
 
     public function testingWhenArticleWithProvidedIdIsPresentInDatabase(): void {
-        $article = new Article();
-        $article->setTitle("Hello World");
-        $article->setContent("Hello world text");
-        
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+
+    $article = new Article();
+    $article->setTitle("Hello World");
+    $article->setContent("Hello world text");
+    
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
 
 
-        $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
+    $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
 
-        $articleRepositoryMock->expects($this->once())
-                              ->method('findOneBy')
-                              ->with(['id' => 1])
-                              ->willReturn($article);
+    $articleRepositoryMock->expects($this->once())
+                            ->method('findOneBy')
+                            ->with(['id' => 1])
+                            ->willReturn($article);
 
-        $response = $articleController->findArticleById(1);
+    $response = $articleController->findArticleById(1);
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertInstanceOf(JsonResponse::class, $response);
 
-        $responseData = json_decode($response->getContent(), true);
+    $responseData = json_decode($response->getContent(), true);
 
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
-        $this->assertEquals($article->getTitle(), $responseData['title']);
-        $this->assertEquals($article->getContent(), $responseData['content']);
+    $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    $this->assertEquals($article->getTitle(), $responseData['title']);
+    $this->assertEquals($article->getContent(), $responseData['content']);
+
     }
 
     public function testFindingAllArticles(): void {
 
-        $firstArticle = new Article();
-        $firstArticle->setTitle("Football");
-        $firstArticle->setContent("Tomorrow we are expecting new game");
+    $firstArticle = new Article();
+    $firstArticle->setTitle("Football");
+    $firstArticle->setContent("Tomorrow we are expecting new game");
 
-        $secondArticle = new Article();
-        $secondArticle->setTitle("Basketball");
-        $secondArticle->setContent("We are playing basketball today");
+    $secondArticle = new Article();
+    $secondArticle->setTitle("Basketball");
+    $secondArticle->setContent("We are playing basketball today");
 
-        $thirdArticle = new Article();
-        $thirdArticle->setTitle("Programming");
-        $thirdArticle->setContent("Symfony is great framework");
+    $thirdArticle = new Article();
+    $thirdArticle->setTitle("Programming");
+    $thirdArticle->setContent("Symfony is great framework");
 
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
 
-        
-        $articleRepositoryMock->expects($this->once())
-        ->method('findAll')
-        ->willReturn([$firstArticle, $secondArticle,$thirdArticle]);
+    
+    $articleRepositoryMock->expects($this->once())
+    ->method('findAll')
+    ->willReturn([$firstArticle, $secondArticle,$thirdArticle]);
 
-        $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
+    $articleController = new ArticleController($articleRepositoryMock,$managerRegistryMock);
 
-        $response = $articleController->findAllArticles();
+    $response = $articleController->findAllArticles();
 
-        $data = json_decode($response->getContent(), true);
+    $data = json_decode($response->getContent(), true);
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertInstanceOf(JsonResponse::class, $response);
 
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
-        $this->assertEquals(3, count($data));
-        $this->assertEquals("Football", $data[0]['title']);
-        $this->assertEquals("Tomorrow we are expecting new game", $data[0]['content']);
-        $this->assertEquals("Basketball", $data[1]['title']);
-        $this->assertEquals("We are playing basketball today", $data[1]['content']);
-        $this->assertEquals("Programming", $data[2]['title']);
-        $this->assertEquals("Symfony is great framework", $data[2]['content']);
+    $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    $this->assertEquals(3, count($data));
+    $this->assertEquals("Football", $data[0]['title']);
+    $this->assertEquals("Tomorrow we are expecting new game", $data[0]['content']);
+    $this->assertEquals("Basketball", $data[1]['title']);
+    $this->assertEquals("We are playing basketball today", $data[1]['content']);
+    $this->assertEquals("Programming", $data[2]['title']);
+    $this->assertEquals("Symfony is great framework", $data[2]['content']);
 
     }
 
-     public function testCreatingNewArticle(): void {
+    public function testCreatingNewArticle(): void {
 
-        $articleData = [
-            'title' => 'Hello World',
-            'content' => 'Hello world content'
-        ];
-        $jsonArticleData = json_encode($articleData);
-    
-        $request = new Request([], [], [], [], [], [], $jsonArticleData);
-    
-        $entityManagerMock = $this->createMock(EntityManagerInterface::class);
-        $entityManagerMock->expects($this->once())->method('persist');
-        $entityManagerMock->expects($this->once())->method('flush');
-    
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
-        $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
-    
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-    
-        $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
-    
-        $response = $articleController->createArticle($request);
-    
-        $this->assertInstanceOf(JsonResponse::class, $response);
-        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+    $articleData = [
+        'title' => 'Hello World',
+        'content' => 'Hello world content'
+    ];
+
+    $jsonArticleData = json_encode($articleData);
+
+    $request = new Request([], [], [], [], [], [], $jsonArticleData);
+
+    $entityManagerMock = $this->createMock(EntityManagerInterface::class);
+    $entityManagerMock->expects($this->once())->method('persist');
+    $entityManagerMock->expects($this->once())->method('flush');
+
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+    $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
+
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+
+    $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
+
+    $response = $articleController->createArticle($request);
+
+    $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
    
     }  
     
+    public function testUpdateArticle(): void {
 
+    $entityManagerMock = $this->createMock(EntityManagerInterface::class);
+    $entityManagerMock->expects($this->once())->method('persist');
+    $entityManagerMock->expects($this->once())->method('flush');
 
-    public function testUpdateArticle() {
-        $entityManagerMock = $this->createMock(EntityManagerInterface::class);
-        $entityManagerMock->expects($this->once())->method('persist');
-        $entityManagerMock->expects($this->once())->method('flush');
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+    $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
 
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
-        $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
+    $data = [
+        'title' => 'Update article',
+        'content' => 'Text for apdating article'
+    ];
 
-        $data = [
-            'title' => 'Update article',
-            'content' => 'Text for apdating article'
-        ];
+    $jsonContent = json_encode($data);
 
-        $jsonContent = json_encode($data);
+    $article = new Article();
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+    $articleRepositoryMock->expects($this->once())->method('findOneBy')->willReturn($article);
 
-        $article = new Article();
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-        $articleRepositoryMock->expects($this->once())->method('findOneBy')->willReturn($article);
+    $request = new Request([], [], [], [], [], [], $jsonContent);
+    $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
 
-        $request = new Request([], [], [], [], [], [], $jsonContent);
-        $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
+    $response = $articleController->updateArticle(1, $request);
 
-        $response = $articleController->updateArticle(1, $request);
+    $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
     }
     public function testDeleteArticle() {
 
-        $entityManagerMock = $this->createMock(EntityManagerInterface::class);
-        $entityManagerMock->expects($this->once())->method('remove');
-        $entityManagerMock->expects($this->once())->method('flush');
+    $entityManagerMock = $this->createMock(EntityManagerInterface::class);
+    $entityManagerMock->expects($this->once())->method('remove');
+    $entityManagerMock->expects($this->once())->method('flush');
 
-        $managerRegistryMock = $this->createMock(ManagerRegistry::class);
-        $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
+    $managerRegistryMock = $this->createMock(ManagerRegistry::class);
+    $managerRegistryMock->expects($this->once())->method('getManager')->willReturn($entityManagerMock);
 
-        $article = new Article();
-        $articleRepositoryMock = $this->createMock(ArticleRepository::class);
-        $articleRepositoryMock->expects($this->once())->method('findOneBy')->willReturn($article);
+    $article = new Article();
+    $articleRepositoryMock = $this->createMock(ArticleRepository::class);
+    $articleRepositoryMock->expects($this->once())->method('findOneBy')->willReturn($article);
 
-        $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
+    $articleController = new ArticleController($articleRepositoryMock, $managerRegistryMock);
 
-        $response = $articleController->deleteArticle(1);
+    $response = $articleController->deleteArticle(1);
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    $this->assertInstanceOf(JsonResponse::class, $response);
+    $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
     }
 
 }
